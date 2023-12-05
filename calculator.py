c8a272631d6f18f05439e2cfa5f1e5b04dc594fa
DS321q <102172410+DS321q@users.noreply.github.com>
2023-12-05 22:15:20 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,41 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+while True:
+    print("\nPlease choose an operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Quit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '5':
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+    elif choice == '4':
+        if num2 == 0:
+            print("Error! Division by zero is not allowed.")
+        else:
+            print(num1, "/", num2, "=", divide(num1, num2))
+    else:
+        print("Invalid input. Please enter a number between 1 and 5.")
